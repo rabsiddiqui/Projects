3ee03e6b5d3764352229714dfc31b99d03ba56bb
rabsiddiqui <rabsiddiqui@yahoo.com>
2016-03-13 15:36:03 -0500

    Added files via upload

---
+++ Name.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class Name {
+	public static void main(String args[]) {
+		String name = "Rabie";
+		
+		for(int a = 0; a < 10; a ++) {
+			System.out.println(name);
+		}
+	}
+}
